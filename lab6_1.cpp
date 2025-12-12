63743afa7a74852b49950ee5e35b263c66da3c4d
Anuntachai-Tew <anunjompang@gmail.com>
2025-12-12 11:12:22 +0700

    Finish

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,7 +2,12 @@
 using namespace std;
 
 int main(){
+    int num ;
+    int num2 ;
+    int mum1 ;
     cout << "Enter an integer: ";
+    cin >> num ;
+    
     cout << "#Even numbers = ";
     cout << "#Odd numbers = ";
     return 0;
